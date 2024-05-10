dddd73a5450ee62f3d3ae69dad58821fefec7022
nuin <nuin@genedrift.org>
2024-05-10 15:14:20 -0600

    Added UI tests with necessary dependencies
    Added a new test module for UI testing, which includes testing the web rendering logic. Also included a config file for these tests and a utility script for parsing HTML. Updated the existing test scripts to incorporate these changes. Necessary dependencies for these tests, such as BeautifulSoup, have been added to the project dependencies.

--- tests/UI/test_ui.py
+++ tests/UI/test_ui.py
@@ -1,3 +1,5 @@
+import click
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -5,55 +7,67 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from tqdm import tqdm
 
-# Set up the browser (replace "chrome" with "firefox" for Firefox)
-browser = webdriver.Chrome()
 
-# Navigate to your webpage
-browser.get("https://blast.alliancegenome.org/blast/SGD/fungal")
+def run_test()
 
-# Locate and click the checkbox
-checkbox = WebDriverWait(browser, 10).until(
-    EC.element_to_be_clickable((By.ID, "Akanthomyces_anchor"))
-)
-checkbox.click()
+    # Set up the browser (replace "chrome" with "firefox" for Firefox)
+    browser = webdriver.Chrome()
 
-# Locate and fill out the input box
-input_box = WebDriverWait(browser, 10).until(
-    EC.element_to_be_clickable((By.NAME, "sequence"))
-)
-input_box.send_keys(
-    "ATGGATTCTGGTATGTTCTAGCGCTTGCACCATCCCATTTAACTGTAAGAAGAATTGCACGGTCCCAATTGCTCGAGAGATTTCTCTTTTACCTTTTTTTACTATTTT"
-    "TCACTCTCCCATAACCTCCTATATTGACTGATCTGTAATAACCACGATATTATTGGAATAAATAGGGGCTTGAAATTTGGAAAAAAAAAAAAAACTGAAATATTTTCGT"
-    "GATAAGTGATAGTGATATTCTTCTTTTATTTGCTACTGTTACTAAGTCTCATGTACTAACATCGATTGCTTCATTCTTTTTGTTGCTATATTATATGTTTAGAGGTTGCT"
-    "GCTTTGGTTATTGATAACGGTTCTGGTATGTGTAAAGCCGGTTTTGCCGGTGACGACGCTCCTCGTGCTGTCTTCCCATCTATCGTCGGTAGACCAAGACACCAAGGTAT"
-)
+    # Navigate to your webpage
+    browser.get("https://blast.alliancegenome.org/blast/SGD/fungal")
 
-# new_tab = WebDriverWait(browser, 10).until(
-#     EC.element_to_be_clickable((By.ID, "toggleNewTab"))
-# )
-# new_tab.click()
-
-
-element = browser.find_element(By.ID, "method")
-element.click()
-
-print("clicked")
-for _ in tqdm(range(20)):  # Pauses the script for 10 seconds
-    time.sleep(1)
-
-try:
-    next_page_element = WebDriverWait(browser, 600).until(
-        EC.presence_of_element_located((By.ID, "view"))
+    # Locate and click the checkbox
+    checkbox = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.ID, "Akanthomyces_anchor"))
     )
-    print("found")
-    browser.save_screenshot('screenshot.png')
-    for _ in tqdm(range(10)):  # Pauses the script for 10 seconds
+    checkbox.click()
+
+    # Locate and fill out the input box
+    input_box = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.NAME, "sequence"))
+    )
+    input_box.send_keys(
+        "ATGGATTCTGGTATGTTCTAGCGCTTGCACCATCCCATTTAACTGTAAGAAGAATTGCACGGTCCCAATTGCTCGAGAGATTTCTCTTTTACCTTTTTTTACTATTTT"
+        "TCACTCTCCCATAACCTCCTATATTGACTGATCTGTAATAACCACGATATTATTGGAATAAATAGGGGCTTGAAATTTGGAAAAAAAAAAAAAACTGAAATATTTTCGT"
+        "GATAAGTGATAGTGATATTCTTCTTTTATTTGCTACTGTTACTAAGTCTCATGTACTAACATCGATTGCTTCATTCTTTTTGTTGCTATATTATATGTTTAGAGGTTGCT"
+        "GCTTTGGTTATTGATAACGGTTCTGGTATGTGTAAAGCCGGTTTTGCCGGTGACGACGCTCCTCGTGCTGTCTTCCCATCTATCGTCGGTAGACCAAGACACCAAGGTAT"
+    )
+
+    # new_tab = WebDriverWait(browser, 10).until(
+    #     EC.element_to_be_clickable((By.ID, "toggleNewTab"))
+    # )
+    # new_tab.click()
+
+
+    element = browser.find_element(By.ID, "method")
+    element.click()
+
+    print("clicked")
+    for _ in tqdm(range(20)):  # Pauses the script for 10 seconds
         time.sleep(1)
-except Exception as e:
-    print(e)
-    browser.quit()
+
+    try:
+        next_page_element = WebDriverWait(browser, 600).until(
+            EC.presence_of_element_located((By.ID, "view"))
+        )
+        print("found")
+        browser.save_screenshot('screenshot.png')
+        for _ in tqdm(range(10)):  # Pauses the script for 10 seconds
+            time.sleep(1)
+    except Exception as e:
+        print(e)
+        browser.quit()
+
+    # print("done")
+    # browser.quit()
+
+@click.command()
+@click.option("-m", "--mod", help="The MOD to test")
+@click.option("-s", "--single_item", help="How many items to test", default=1)
+def prepare_test(mod, single_item ):
+
+    con
 
 
-# print("done")
-# browser.quit()
-
+if __name__ == "__main__":
+    check_names()
---
+++ tests/UI/utils.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <filename>")
+        return
+
+    filename = sys.argv[1]
+
+    with open(filename, 'r') as f:
+        data = f.read()
+
+    soup = BeautifulSoup(data, features="html.parser")
+    # print(soup)
+
+    items = []
+    for tag in soup.find_all('a', class_='jstree-anchor'):
+        # print(tag.text)
+        # print(tag.get("id"))
+        items.append(tag.get("id"))
+
+    print(items)
+
+if __name__ == "__main__":
+    main()
