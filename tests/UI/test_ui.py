bddd2cae96593ec9e7bf2ca28c29a60ab127f3bb
nuin <nuin@genedrift.org>
2024-05-15 11:47:29 -0600

    Update UI tests for better organization and readability
    The commit refactors and optimizes the UI tests for a better presentation of test actions. The changes provide detailed logging about test progress, including the items being tested and actions taken. Additionally, the importing from rich library and test definitions are updated for readability and better code organization. Finally, a new option is introduced for testing specific molecule types.

--- tests/UI/test_ui.py
+++ tests/UI/test_ui.py
@@ -2,7 +2,7 @@ import json
 import time
 
 import click
-from rich import Console
+from rich.console import Console
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,31 +12,34 @@ from tqdm import tqdm
 console = Console()
 
 
-def run_test(mod, items, db_type, sequence):
-    # Set up the browser (replace "chrome" with "firefox" for Firefox)
-    browser = webdriver.Chrome()
-
-    # Navigate to your webpage
-    browser.get(f"https://blast.alliancegenome.org/blast/{mod}/{type}")
+def run_test(mod, items, type, sequence):
 
     # Locate and click the checkbox
     for item in items:
+        # Set up the browser (replace "chrome" with "firefox" for Firefox)
+        browser = webdriver.Chrome()
+
+        # Navigate to your webpage
+        browser.get(f"https://blast.alliancegenome.org/blast/{mod}/{type}")
+        console.log(f"Testing {item}")
         checkbox = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.ID, "Akanthomyces_anchor"))
+            EC.element_to_be_clickable((By.ID, item))
         )
         checkbox.click()
+        console.log(f"Clicked {item}")
 
         # Locate and fill out the input box
         input_box = WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.NAME, "sequence"))
         )
         input_box.send_keys(sequence)
+        console.log(f"Sent sequence")
 
         element = browser.find_element(By.ID, "method")
         element.click()
+        console.log("Clicked button")
 
-        print("clicked")
-        for _ in tqdm(range(10)):  # Pauses the script for 10 seconds
+        for _ in tqdm(range(5)):  # Pauses the script for 10 seconds
             time.sleep(1)
 
         try:
@@ -44,27 +47,31 @@ def run_test(mod, items, db_type, sequence):
                 EC.presence_of_element_located((By.ID, "view"))
             )
             browser.save_screenshot(f"{item}.png")
-            for _ in tqdm(range(10)):  # Pauses the script for 10 seconds
+            for _ in tqdm(range(15)):  # Pauses the script for 10 seconds
                 time.sleep(1)
         except Exception as e:
             console.log(e)
-            browser.quit()
+
+        browser.quit()
 
 
 @click.command()
 @click.option("-m", "--mod", help="The MOD to test")
 @click.option("-t", "--type", help="The DB type to test, i.e. fungal for SGD")
 @click.option("-s", "--single_item", help="How many items to test", default=1)
+@click.option("-M", "--molecule", help="The molecule to test, nucl or prot only, default nucl", default="nucl")
 @click.option(
     "-n",
     "--number_of_items",
     help="number of items to test, random, default all, cannot be used with single item",
 )
-def prepare_test(mod, type, single_item, number_of_items):
+def prepare_test(mod, type, single_item, number_of_items, molecule):
     """ """
 
+    # print(mod, type, single_item, number_of_items, molecule)
+
     data = json.loads(open("config.json").read())
-    run_test(mod, data[mod][type]["items"], type, data[mod][type]["sequence"])
+    run_test(mod, data[mod][type]["items"], type, data[mod][type][molecule])
 
 
 if __name__ == "__main__":
