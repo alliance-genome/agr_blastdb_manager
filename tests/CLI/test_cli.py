969904f28601fcd3e1df443e8f01a69dcf261a0a
nuin <nuin@genedrift.org>
2024-06-04 19:38:43 -0600

    Add new SGD test configuration file
    This commit adds the new test configuration file config.json for the SGD test suite. The configuration file details various server endpoints and their database records for testing purposes. It includes both main and fungal databases, each with their specific URLs and nucleotide and protein sequences.

---
+++ tests/CLI/test_cli.py
@@ -0,0 +1,44 @@
+# blastn -db
+# /Users/nuin/Projects/wormbase/blast/agr_blastdb_manager/data/blast/SGD/fungal/databases/Zygosaccharomyces/rouxii/Z_rouxii_Genome_Assembly/zygosaccharomyces_rouxii_genomicdb
+# -query input_files/SGD.fasta                               │
+
+
+import pathlib
+import click
+import json
+
+
+
+def run_blast(db, fasta):
+
+    print(db)
+    temp = open("temp.fasta", "w")
+    temp.write(fasta)
+    temp.close()
+
+    blast_command = f"blastn -db {db} -query temp.fasta"
+
+    print(blast_command)
+
+
+@click.command()
+@click.option("-d", "--datadir", help="The path to the data directory", required=True)
+@click.option("-M", "--mod", help="The MOD to test", required=True)
+@click.option("-e", "--environment", help="The environment to test", required=True)
+def setup_blast(datadir, mod, environment):
+
+    data = json.loads(open("config.json").read())
+
+    p = pathlib.Path(f"{datadir}/{mod}/{environment}/databases/")
+    dbs = data[mod][environment]["dbs"]
+    fasta = f">test\nd{data[mod][environment]["nucl"]}"
+
+
+    for db in dbs:
+        full_dir = p / db
+        run_blast(full_dir, fasta)
+
+
+
+if __name__ == "__main__":
+    setup_blast()
